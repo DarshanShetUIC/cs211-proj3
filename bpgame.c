ddb95b86eb10e15450c4483f7cf6ee323f893968
DarshanShetUIC <dshet2@uic.edu>
2022-02-26 17:17:01 -0600

    successfully compiles with make

--- bpgame.c
+++ bpgame.c
@@ -4,12 +4,15 @@
 
 
 /** #include statements... **/
+#include <stdio.h>
+#include <stddef.h>
 
+#include "bpgame.h"
 
 /** TYPEDEFS / STRUCTs HERE ***/
 
 
-struct bpgame {
+typedef struct bpgame {
 
 
    // YOU DECIDE WHAT TO PUT IN HERE TO CAPTURE THE STATE OF
@@ -19,10 +22,10 @@ struct bpgame {
 	
 	int score;
 	
-	BPGame* prev;
 	BPGame* next;
+	BPGame* prev;
 	
-	int** arr;
+	char** arr;
 
 } BPGame;
 
@@ -33,10 +36,49 @@ struct bpgame {
 /*** IMPLEMENTATION OF bp_XXXX FUNCTIONS HERE  ****/
 
 BPGame * bp_create(int nrows, int ncols){
-	// Check comments
 	if (nrows < 0 || nrows > 40 || ncols < 0 || ncols > 40){
-		fprintf(stderr, "%s", "nrows or ncols out of bounds!!!");
+		printf("nrows or ncols out of bounds!!!");
 		return NULL;
 	}
-	
+	return NULL;
+}
+
+BPGame * bp_create_from_mtx(char mtx[][MAX_COLS], int nrows, int ncols){
+	return NULL;
+}
+
+void bp_destroy(BPGame * b){
+	return;
+}
+
+void bp_display(BPGame * b){
+	return;
+}
+
+int bp_pop(BPGame * b, int r, int c){
+	return 0;
+}
+
+int bp_is_compact(BPGame * b){
+	return 0;
+}
+
+void bp_float_one_step(BPGame * b){
+	return;
+}
+
+int bp_score(BPGame * b){
+	return 0;
+}
+
+int bp_get_balloon(BPGame * b, int r, int c){
+	return 0;
+}
+
+int bp_can_pop(BPGame * b){
+	return 0;
+}
+
+int bp_undo(BPGame * b){
+	return 0;
 }
